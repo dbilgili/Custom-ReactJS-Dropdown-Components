36565af17b7a0e5fe71856436d0d51f68b0ec408
Dogacan Bilgili <dbilgili@sabanciuniv.edu>
2018-12-18 23:15:39 +0300

    remove react-onclickoutside package dependency

--- src/components/DropdownMultiple.js
+++ src/components/DropdownMultiple.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome';
-import onClickOutside from "react-onclickoutside";
-import '../styles/global.css';
+import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+import '../style/global.styl'
 
 class DropdownMultiple extends Component{
   constructor(props){
     super(props)
     this.state = {
       listOpen: false,
-      headerTitle: this.props.title
+      headerTitle: this.props.title,
+      timeOut: null
     }
+    this.close = this.close.bind(this)
+  }
+
+  componentDidUpdate(){
+    const { listOpen } = this.state
+    setTimeout(() => {
+      if(listOpen){
+        window.addEventListener('click', this.close)
+      }
+      else{
+        window.removeEventListener('click', this.close)
+      }
+    }, 0)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('click', this.close)
+  }
+
+  close(timeOut){
+    this.setState({
+      listOpen: false
+    })
   }
 
   static getDerivedStateFromProps(nextProps){
@@ -25,12 +48,6 @@ class DropdownMultiple extends Component{
     }
   }
 
-  handleClickOutside(){
-    this.setState({
-      listOpen: false
-    })
-  }
-
   toggleList(){
     this.setState(prevState => ({
       listOpen: !prevState.listOpen
@@ -42,14 +59,14 @@ class DropdownMultiple extends Component{
     const{listOpen, headerTitle} = this.state
     return(
       <div className="dd-wrapper">
-      <div className="dd-header" onClick={() => this.toggleList()}>
-          <div className="dd-header-title">{headerTitle}</div>
-          {listOpen
-            ? <FontAwesome name="angle-up" size="2x"/>
-            : <FontAwesome name="angle-down" size="2x"/>
-          }
-      </div>
-       {listOpen && <ul className="dd-list">
+        <div className="dd-header" onClick={() => this.toggleList()}>
+            <div className="dd-header-title">{headerTitle}</div>
+            {listOpen
+              ? <FontAwesome name="angle-up" size="2x"/>
+              : <FontAwesome name="angle-down" size="2x"/>
+            }
+        </div>
+       {listOpen && <ul className="dd-list" onClick={e => e.stopPropagation()}>
          {list.map((item) => (
            <li className="dd-list-item" key={item.title} onClick={() => toggleItem(item.id, item.key)}>
              {item.title} {item.selected && <FontAwesome name="check"/>}
@@ -62,4 +79,4 @@ class DropdownMultiple extends Component{
 
 }
 
-export default onClickOutside(DropdownMultiple);
+export default DropdownMultiple
