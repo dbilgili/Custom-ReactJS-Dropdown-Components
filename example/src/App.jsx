40eeac73475ae4e860f9082fc1f4cbb858178652
Doğacan Bilgili <dbilgili@sabanciuniv.edu>
2020-10-13 20:31:15 +0200

    Use JSX as extension

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -2,140 +2,140 @@ import React, { Component } from 'react';
 import { DropdownMultiple, Dropdown } from 'reactjs-dropdown-component';
 
 class App extends Component {
-    constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
       location: [
         {
           id: 0,
           title: 'New York',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 1,
           title: 'Dublin',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 2,
           title: 'California',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 3,
           title: 'Istanbul',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 4,
           title: 'Izmir',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 5,
           title: 'Oslo',
           selected: false,
-          key: 'location'
+          key: 'location',
         },
         {
           id: 6,
           title: 'Zurich',
           selected: false,
-          key: 'location'
-        }
+          key: 'location',
+        },
       ],
       fruit: [
         {
           id: 0,
           title: 'Apple',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 1,
           title: 'Orange',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 2,
           title: 'Grape',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 3,
           title: 'Pomegranate',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 4,
           title: 'Strawberry',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 5,
           title: 'Banana',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 6,
           title: 'Blueberry',
           selected: false,
-          key: 'fruit'
+          key: 'fruit',
         },
         {
           id: 7,
           title: 'Watermelon',
           selected: false,
-          key: 'fruit'
-        }
-      ]
-    }
+          key: 'fruit',
+        },
+      ],
+    };
   }
 
   componentDidMount() {
-    window.addEventListener("keydown", this.tabKeyPressed);
-    window.addEventListener("mousedown", this.mouseClicked);
+    window.addEventListener('keydown', this.tabKeyPressed);
+    window.addEventListener('mousedown', this.mouseClicked);
   }
 
   tabKeyPressed = (e) => {
     if (e.keyCode === 9) {
-      document.querySelector('body').classList.remove("noFocus")
+      document.querySelector('body').classList.remove('noFocus');
       window.removeEventListener('keydown', this.tabKeyPressed);
       window.addEventListener('mousedown', this.mouseClicked);
     }
   }
 
   mouseClicked = (e) => {
-    document.querySelector('body').classList.add("noFocus")
+    document.querySelector('body').classList.add('noFocus');
     window.removeEventListener('mousedown', this.mouseClicked);
     window.addEventListener('keydown', this.tabKeyPressed);
   }
 
   toggleItem = (id, key) => {
-    let temp = JSON.parse(JSON.stringify(this.state[key]))
-    temp[id].selected = !temp[id].selected
+    const temp = JSON.parse(JSON.stringify(this.state[key]));
+    temp[id].selected = !temp[id].selected;
     this.setState({
-      [key]: temp
-    })
+      [key]: temp,
+    });
   }
 
   resetThenSet = (id, key) => {
-    let temp = JSON.parse(JSON.stringify(this.state[key]))
-    temp.forEach(item => item.selected = false);
+    const temp = JSON.parse(JSON.stringify(this.state[key]));
+    temp.forEach((item) => item.selected = false);
     temp[id].selected = true;
     this.setState({
-      [key]: temp
-    })
+      [key]: temp,
+    });
   }
 
   render() {
@@ -164,7 +164,7 @@ class App extends Component {
 
         <div className="wrapper">
           <DropdownMultiple
-            searchable={["Search for location", "No matching location"]}
+            searchable={['Search for location', 'No matching location']}
             titleHelper="Location"
             title="Select location"
             list={this.state.location}
@@ -172,7 +172,7 @@ class App extends Component {
           />
 
           <Dropdown
-            searchable={["Search for fruit", "No matching fruit"]}
+            searchable={['Search for fruit', 'No matching fruit']}
             title="Select fruit"
             list={this.state.fruit}
             resetThenSet={this.resetThenSet}
