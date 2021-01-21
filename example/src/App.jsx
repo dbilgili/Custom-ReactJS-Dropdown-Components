f9dd2d29c4bf42eb3f3d445da210372fa3455a91
Doğacan Bilgili <dbilgili@sabanciuniv.edu>
2021-01-21 03:12:13 +0100

    Remove closeOnSelection from the demo

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -70,7 +70,6 @@ class App extends Component {
             titleSingular="Location"
             title="Select locations"
             list={locations}
-            closeOnSelection
             onChange={this.onChange}
           />
 
